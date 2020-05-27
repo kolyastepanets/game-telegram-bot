eec1a9f0072df17c0c9f1f8d3504c035b962b2cc
Mykola Stepanets <mykolastepanets@ip-192-168-0-102.us-east-2.compute.internal>
2020-05-27 23:09:53 +0300

    show user games in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register User do
   permit_params :nickname, :time_to_play
+
+  show do
+    attributes_table_for user do
+      row :nickname
+      row :time_to_play
+      row :tg_id
+      row :tg_first_name
+      row :tg_last_name
+      row :tg_username
+      row :is_bot
+      row :tg_language_code
+      row :created_at
+      row :updated_at
+      row :has_blocked_bot
+
+      table_for user.games do
+        column :name
+        column :platform
+      end
+    end
+  end
 end
